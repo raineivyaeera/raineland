4cd6cbbdeb4388477b255689b96fabbcee13f042
raineivyaeera <121701316+raineivyaeera@users.noreply.github.com>
2024-02-27 23:22:17 -0700

    update fix!

--- jukebox.js
+++ jukebox.js
@@ -26,34 +26,6 @@ const songs =[
     "https://github.com/raineivyaeera/Lines-Of-Thought-Website/blob/a789258dd63d73d3ad32869646c1b12603df8411/_Jukebox%20Portfolio/WTSOILAPB.mp3?raw=true",
     "https://github.com/raineivyaeera/Lines-Of-Thought-Website/blob/a789258dd63d73d3ad32869646c1b12603df8411/_Jukebox%20Portfolio/Watching%20The%20Waves.mp3?raw=true",
     "https://github.com/raineivyaeera/Lines-Of-Thought-Website/blob/a789258dd63d73d3ad32869646c1b12603df8411/_Jukebox%20Portfolio/Wordends.mp3?raw=true"
-    
-    // "_Jukebox Portfolio/10 Minute Old Ketchup Packet.mp3",
-    // "_Jukebox Portfolio/Beary Good Day.mp3",
-    // "_Jukebox Portfolio/Blood Moon.mp3",
-    // "_Jukebox Portfolio/Casscifers Confrontation.mp3",
-    // "_Jukebox Portfolio/Creepies & Crawlies.mp3",
-    // "_Jukebox Portfolio/Deathroll.mp3",
-    // "_Jukebox Portfolio/Decorating Time.mp3",
-    // "_Jukebox Portfolio/Endless & Bouncy.mp3",
-    // "_Jukebox Portfolio/Finding Confidence.mp3",
-    // "_Jukebox Portfolio/FTL Inspired (Min-Max Distortion).mp3",
-    // "_Jukebox Portfolio/Good Idea.mp3",
-    // "_Jukebox Portfolio/Lacrymose Head.mp3",
-    // "_Jukebox Portfolio/Lightkeeper.mp3",
-    // "_Jukebox Portfolio/Logging Off!.mp3",
-    // "_Jukebox Portfolio/Magician.mp3",
-    // "_Jukebox Portfolio/Ode To Mort Garson.mp3",
-    // "_Jukebox Portfolio/Other Views.mp3",
-    // "_Jukebox Portfolio/Present.mp3",
-    // "_Jukebox Portfolio/Radience.mp3",
-    // "_Jukebox Portfolio/Self Limitation.mp3",
-    // "_Jukebox Portfolio/Shoreline.mp3",
-    // "_Jukebox Portfolio/Space Debris.mp3",
-    // "_Jukebox Portfolio/Sweet Victory.mp3",
-    // "_Jukebox Portfolio/V-ish.mp3",
-    // "_Jukebox Portfolio/Watching The Waves.mp3",
-    // "_Jukebox Portfolio/Wordends.mp3",
-    // "_Jukebox Portfolio/WTSOILAPB.mp3"
 ];
 
 function shuffleArray(array) {
@@ -69,6 +41,7 @@ let currentIndex = 0;
 
 function randomSong() {
     let audioPlayer = document.getElementById("audio");
+    audioPlayer.volume = 0.75;
 
     if (currentIndex >= shuffledSongs.length) {
         shuffledSongs = shuffleArray([...songs]);
