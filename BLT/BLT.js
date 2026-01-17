94f4e1033917fd9d7f67f6c45b5416eb891d2a84
Raine Eera <raineivyaeera@gmail.com>
2026-01-17 05:52:39 -0700

    WAH

--- BLT/BLT.js
+++ BLT/BLT.js
@@ -12,24 +12,22 @@ function pause() { worker.postMessage('pausetimer'); }
 
 worker.onmessage = function(event) {
     const { message, timertotal, mode } = event.data;
-
     if (message === "timersound") {
         timersound.currentTime = 0;
         timersound.play();
         return;
     }
-
     if (message === "update") {
         let seconds = timertotal % 60;
         let minutes = Math.floor(timertotal / 60);
         if (seconds < 10) seconds = "0" + seconds;
 
-        timertext.innerHTML = minutes + ":" + seconds + (mode === "up" ? " ↑" : " ↓");
+        timertext.innerHTML = minutes + ":" + seconds;
     }
 };
 
 document.addEventListener('keypress', (e) => {
-    if (e.key === 'c' || 'r' || 's') {
+    if (e.key === 'c' || 'r' || 's')
         worker.postMessage('togglemode');
     }
 });
